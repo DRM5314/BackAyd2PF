e726841424eafbd3e497a508d1e8fd8731631332
DRM5314 <davidrodolfo-martinezmiranda@cunoc.edu.gt>
2024-07-07 18:19:42 -0600

    add anotation repository

--- src/main/java/com/library/repository/UserRepository.java
+++ src/main/java/com/library/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.library.repository;
 
 import com.library.model.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+@Repository
 public interface UserRepository extends CrudRepository<User,Long> {
     Optional <User> findByUsername(String userName);
 }
