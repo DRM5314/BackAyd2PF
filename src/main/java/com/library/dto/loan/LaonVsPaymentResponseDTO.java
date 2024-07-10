fad476cf97bfc0ec71f198e1cb46cbedd78464a9
DRM5314 <davidrodolfo-martinezmiranda@cunoc.edu.gt>
2024-07-10 00:31:16 -0600

    Report payments for student

---
+++ src/main/java/com/library/dto/loan/LaonVsPaymentResponseDTO.java
@@ -0,0 +1,19 @@
+package com.library.dto.loan;
+
+import com.library.enums.PaymentEnum;
+import com.library.model.Loan;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDate;
+@Getter
+public class LaonVsPaymentResponseDTO {
+    private LoanResponseDTO loan;
+    private LocalDate datePayment;
+    private PaymentEnum type;
+    public LaonVsPaymentResponseDTO(Loan loan, LocalDate datePayment, PaymentEnum type) {
+        this.loan = new LoanResponseDTO(loan);
+        this.datePayment = datePayment;
+        this.type = type;
+    }
+}
--- src/main/java/com/library/dto/loan/ReportMoreStudentResponseDTO.java
+++ src/main/java/com/library/dto/loan/ReportMoreStudentResponseDTO.java
@@ -1,19 +1,20 @@
 package com.library.dto.loan;
 
-import com.library.model.Loan;
 import com.library.model.Payment;
 import com.library.model.Student;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Getter
 public class ReportMoreStudentResponseDTO {
     private final Student carnet;
-    private final List<Payment> paymens;
+    private final List<LaonVsPaymentResponseDTO> loans;
     private final Double totalCash;
     public ReportMoreStudentResponseDTO(List<Payment> paymens){
         this.carnet = paymens.get(0).getLoan().getCarnet();;
-        this.paymens = paymens;
+        this.loans = paymens.stream().map(payment -> new LaonVsPaymentResponseDTO(payment.getLoan(), payment.getDatePayment(), payment.getType())).collect(Collectors.toList());
         this.totalCash = paymens.stream().mapToDouble(Payment::getTotal).sum();;
     }
 
